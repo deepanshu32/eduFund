8bc38c6c983b1846144ee84bb3ad15178f73bc94
Deepanshu Garg <deepanshu@Deepanshus-MacBook-Pro.local>
2021-01-28 20:18:46 +0530

    update: updated validation messges

--- middleware/validator.js
+++ middleware/validator.js
@@ -13,13 +13,13 @@ module.exports.jwtTokenValidator = [
 module.exports.registerValidator = [
     check("firstName").isAlpha().isLength({ min: 2, max: 50 }).withMessage("First Name is invalid!"),
     check("lastName").isAlpha().isLength({ min: 1, max: 50 }).withMessage("Last Name is invalid!"),
-    check("email").isEmail().isLength({ min: 5, max: 50 }).withMessage("Email is invalid!"),
-    check("password").isLength({ min: 6 }).withMessage("Password is invalid!"),
-    check("mobile").isMobilePhone().isLength({ min: 10, max: 10 }).withMessage("Mobile is invalid!")
+    check("email").isEmail().withMessage("Email is invalid!").isLength({ min: 5, max: 50 }).withMessage("Email is invalid!"),
+    check("password").isLength({ min: 6 }).withMessage("Password should be of minimum 6 characters!"),
+    check("mobile").isMobilePhone().withMessage("Mobile number is invalid!")
 ]
 
 /* User Log in validation */
 module.exports.loginInValidator = [
     check("email").isEmail().withMessage("Email is invalid!"),
-    check("password").isLength({ min: 6 }).withMessage("Password is invalid!")
+    check("password").isLength({ min: 6 }).withMessage("Password should be of minimum 6 characters!")
 ]
--- models/users.js
+++ models/users.js
@@ -116,7 +116,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
   }
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
-    throw new Error("Invalid password!");
+    throw new Error("Incorrect password!");
   }
   return user;
 };
